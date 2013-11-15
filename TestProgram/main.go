f9b7a631f2c745591d003917737747c635a28f2c
Morten Siebuhr <sbhr@sbhr.dk>
2013-11-15 11:50:52 +0100

    Include pre-made import statements in the test program.

--- TestProgram/main.go
+++ TestProgram/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/msiebuhr/go-tag-test"
+	"github.com/msiebuhr/go-tag-test" // Whatever is on master
+	//"github.com/msiebuhr/go-tag-test@master" // Ditto
+	//"github.com/msiebuhr/go-tag-test@v0.0.0" // Tag v0.0.0 / First release
+	//"github.com/msiebuhr/go-tag-test@v0.0.1" // Tag v0.0.1 / Second release
 )
 
 func main() {
